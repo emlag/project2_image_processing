89eb3d59969f14fd2654c742357438fb5966b775
CS106A <>
2019-02-15 12:57:47 -0800

    {"types":"C","runs":17}

--- DarkRoomAlgorithms.java
+++ DarkRoomAlgorithms.java
@@ -12,7 +12,7 @@
 import acm.graphics.*;
 
 public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
-	
+
 	public GImage rotateLeft(GImage source) {
 		// TODO
 		return null;
@@ -24,144 +24,33 @@ public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
 	}
 
 	public GImage flipHorizontal(GImage source) {
-		int[][] pixelArray = source.getPixelArray();
-		int width = pixelArray[0].length;
-		for (int col1 = 0; col1 < width/2; col1++) {
-			int col2 = width - col1 - 1;
-			for (int row = 0; row < pixelArray.length; row++) {
-				int temp = pixelArray[row][col1];
-				pixelArray[row][col1] = pixelArray[row][col2];
-				pixelArray[row][col2] = temp;
-			}
-		}
-		return new GImage(pixelArray);
+		// TODO
+		return null;
 	}
-	
+
 	public GImage negative(GImage source) {
-		int[][] pixelArray = source.getPixelArray();  
-		for (int r = 0; r < pixelArray.length; r++) {
-			for(int c = 0; c < pixelArray[0].length; c++) {
-				int pixel = pixelArray[r][c];
-				int redValue = GImage.getRed(pixel);
-				int greenValue = GImage.getGreen(pixel);
-				int blueValue = GImage.getBlue(pixel);
-				int newRed = Math.abs(255 - redValue);
-				int newGreen = Math.abs(255 - greenValue);
-				int newBlue = Math.abs(255 - blueValue);
-				int newPixel = GImage.createRGBPixel(newRed, newGreen, newBlue);
-				pixelArray[r][c] = newPixel;
-			}
-		}
-		GImage image = new GImage(pixelArray); 
-		return image;
+		// TODO
+		return null;
 	}
-	
+
 	public GImage greenScreen(GImage source) {
-		int[][] pixelArray = source.getPixelArray(); 
-		for (int r = 0; r < pixelArray.length; r++) {
-			for(int c = 0; c < pixelArray[0].length; c++) {
-				int pixel = pixelArray[r][c];
-				int redValue = GImage.getRed(pixel);
-				int greenValue = GImage.getGreen(pixel);
-				int blueValue = GImage.getBlue(pixel);
-				int alphaValue = GImage.getAlpha(pixel);
-				int bigger = Math.max(redValue, blueValue);
-				if (greenValue >= 2 * bigger) {
-					alphaValue = 0;
-				}
-				int newPixel = GImage.createRGBPixel(redValue, greenValue, blueValue, alphaValue);
-				pixelArray[r][c] = newPixel;
-			}
-		}
-		return new GImage(pixelArray); 
+		// TODO
+		return null;
 	}
 
 	public GImage equalize(GImage source) {
-		int[][] pixelArray = source.getPixelArray();
-		int[] histogram = computeHistogram(pixelArray);
-		int[] cumulativeHistogram = computeCumulativeHistogram(histogram);
-		equalizeImage(pixelArray, cumulativeHistogram);
-
-		return new GImage(pixelArray);
+		// TODO
+		return null;
 	}
 
-	private int[] computeHistogram(int[][] pixels) {
-		int height = pixels.length;
-		int width = pixels[0].length;
-
-		int[] histogram = new int[256];
-
-		// Fill in the histogram using all the pixel's luminosities
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				int pixel = pixels[row][col];
-				int red = GImage.getRed(pixel);
-				int green = GImage.getGreen(pixel);
-				int blue = GImage.getBlue(pixel);
-				int luminosity = computeLuminosity(red, green, blue);
-
-				histogram[luminosity]++;
-			}
-		}
-
-		return histogram;
-	}
-
-	private int[] computeCumulativeHistogram(int[] histogram) {
-		int[] cumulativeHistogram = new int[histogram.length];
-
-		// Index i in the cumulative histogram is the number of pixels
-		// with AT MOST luminosity i.
-		int total = 0;
-		for (int i = 0; i < histogram.length; i++) {
-			total += histogram[i];
-			cumulativeHistogram[i] = total;
-		}
-
-		return cumulativeHistogram;
-	}
-
-	private void equalizeImage(int[][] array, int[] cumulativeHistogram) {
-		for (int row = 0; row < array.length; row++) {
-			for (int col = 0; col < array[0].length; col++) {
-					int pixel = array[row][col];
-					int red = GImage.getRed(pixel);
-					int green = GImage.getGreen(pixel);
-					int blue = GImage.getBlue(pixel);
-					int luminosity = computeLuminosity(red, green, blue);
-
-					int newLuminosity = (int) (255 * cumulativeHistogram[luminosity] / (array.length * array[0].length));
-					array[row][col] = GImage.createRGBPixel(newLuminosity, newLuminosity, newLuminosity);
-			}
-		}
-	}
 
 	public GImage crop(GImage source, int cropX, int cropY, int cropWidth, int cropHeight) {
-		int[][] pixelArray = source.getPixelArray();
-		int[][] croppedArray = new int[cropHeight][(int) cropWidth];
-		for (int r = 0; r < croppedArray.length; r++) {
-			for (int c = 0; c < croppedArray[0].length; c++) {
-				croppedArray[r][c] = pixelArray[(r + cropY)][(c + cropX)];
-			}
-		}
-		return new GImage(croppedArray);
+		// TODO
+		return null;
 	}
 
 	public GImage scale(GImage source, double scaleFactor) {
-		int[][] originalPixelArray = source.getPixelArray();
-		int height = originalPixelArray.length;
-		int width = originalPixelArray[0].length;
-		int newHeight = (int) (height * scaleFactor);
-		int newWidth = (int) (width * scaleFactor);
-		int[][] newPixelArray = new int[newHeight][newWidth];
-
-		for (int r = 0; r < newPixelArray.length; r++) {
-			for (int c = 0; c < newPixelArray[0].length; c++) {
-				int color = originalPixelArray[(int) (r / (double) newHeight * height)][(int) (c / (double) newWidth
-						* width)];
-				newPixelArray[r][c] = color;
-			}
-		}
-		return new GImage(newPixelArray);
+		// TODO
+		return null;
 	}
 }
